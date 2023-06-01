14a0637d0e88da2a70a0a7c5c4eb588dc1e79a4c
Eskil Lindløv Sand <eskilsand@outlook.com>
2023-06-01 21:42:23 +0200

    Update blog post

--- src/components/home/ShortDescription.tsx
+++ src/components/home/ShortDescription.tsx
@@ -2,8 +2,8 @@ export function ShortDescription() {
     return (
       <div className="mb-8 mt-4">
         <p>I started learning to code in 2014 and have been hooked ever since.</p>
-        <p>I landed my first job as a frontend developer in 2018</p>
-        <p>I have a passion for JS/TS, react and .net</p>
+        <p>I landed my first job as a developer in 2018</p>
+        <p>I have a passion for JS/TS, React and .NET</p>
         <p>I started this blog to document and share my knowledge & experience.</p>
       </div>
     )
