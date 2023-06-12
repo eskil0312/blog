bb640e93695693363ac853eb6ab0a2079ce0458e
Eskil Lindløv Sand <eskilsand@outlook.com>
2023-06-12 21:36:28 +0200

    Removed unused links

--- src/data/headerNavLinks.ts
+++ src/data/headerNavLinks.ts
@@ -1,7 +1,4 @@
 export let headerNavLinks = [
-    { href: '/blog', title: 'Blog' },
-    { href: '/snippets', title: 'Snippets' },
-    { href: '/projects', title: 'Projects' },
-    { href: '/about', title: 'About' },
-    { href: '/resume', title: 'Resume' },
-  ]
+  { href: '/blog', title: 'Blog' },
+  { href: '/about', title: 'About' },
+]
